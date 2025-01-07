f16f5f77eafc64f1c5c3e3d4ad74823ebd3ac75f
Muruts-hagos <muruts.hagos@le-campus-numerique.fr>
2025-01-07 13:52:03 +0100

    3me midif

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,15 @@
 // you can write js here
 console.log('exo-2');
+const myDate = new Date();
+const day = myDate.getDay();
+// const day = 0;
+
+
+console.log(day);
+
+if (day==6||day==0) {
+    console.log("weekend");
+} else { 
+    console.log("weekday")
+}
+   
