22f88cee5bda0e0d5f9959d9361cc1888783f53d
Muruts-hagos <muruts.hagos@le-campus-numerique.fr>
2025-01-08 15:06:14 +0100

    EXO5

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,16 @@
 // you can write js here
 
 console.log('exo-5');
+let input = 'Turpentine and turtles'
+let vowels = ['A', 'E', 'I', 'O', 'U']
+let resultArray = []
+for (let i = 0; i < input.length; i++) {
+
+    let character = input[i].toUpperCase();
+    if (vowels.indexOf(character)!== -1) {
+        resultArray.push(character)
+    }
+}
+let result = resultArray.join()
+console.log(result)
+console.log('only small metter ',resultArray)
