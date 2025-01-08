733295033a3006cd2abf4aaa599228a0fe5f889d
Muruts-hagos <muruts.hagos@le-campus-numerique.fr>
2025-01-08 09:35:10 +0100

    exo3

--- exo-3/exo3.js
+++ exo-3/exo3.js
@@ -1,2 +1,75 @@
 // you can write js here
-console.log('exo-3');
+let playerInput = prompt("Your choice ( rock, paper, scissors) ?")
+
+function getPlayerChoice(playerInput) {
+    playerInput = playerInput.toLowerCase()
+
+    if (playerInput == "rock" || playerInput == "paper" || playerInput == "scissors") {
+        return playerInput
+    } else {
+        console.error("invalid input")
+        return null
+    }
+}
+
+
+
+
+
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+
+    if (randomNumber === 0) {
+        return "rock";
+    } else if (randomNumber === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+function findWinner(playerChoice, computerChoice) {
+    if (playerChoice == computerChoice) {
+        return "Tied";
+    }
+    if (playerChoice == "rock") {
+        if (computerChoice == "scissors") {
+            return 'win'
+        } else {
+            return 'lost'
+        }
+
+    }
+
+
+
+
+    if (playerChoice == "paper") {
+        if (computerChoice == "rock") {
+            return 'win'
+        } else {
+            return 'lost'
+        }
+    }
+    if (playerChoice === 'scissors') {
+        if (computerChoice === 'paper') {
+            return 'Won';
+        } else {
+            return 'Lost';
+        }
+    }
+}
+
+function playGame() {
+    let uchoice = getPlayerChoice(playerInput);
+    let cChoice = getComputerChoice();
+    if (uchoice === null) {
+        return;
+
+    }
+    
+    console.log(uchoice,cChoice) ;
+    const result = findWinner(uchoice ,cChoice)
+    console.log(result)
+}
+playGame()
